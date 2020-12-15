a41e8ab14ce9664bf34ec0b9ebe4d549263ade6f
xing <172176234@qq.com>
2020-12-15 16:24:32 +0800

    update

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,3 +3,7 @@ import request from '@/utils/request';
 export function queryList(params: any) {
   return request.get('/example/list', { params });
 }
+
+export function post(params: any) {
+  return request('/example/list', { params, method: 'POST' });
+}
---
+++ src/utils/request.ts
@@ -0,0 +1,40 @@
+import request from 'gdin-request';
+import config from '@/constants/config';
+import { notification } from 'antd';
+import storage, { storageKeys } from '@/utils/storage';
+import { history } from 'umi';
+
+// 第一次进来页面
+let firstEnter = true;
+
+request.base.prefix = config.hostApi;
+request.base.skipErrorMessage = config.request.skipErrorMessage;
+request.base.dataMode = config.request.dataMode;
+request.base.getAuthorization = () => {
+  // 登录信息
+  const tokenType =
+    storage.get(storageKeys.sysTokenType) || config.auth.tokenType;
+  const accessToken =
+    storage.get(storageKeys.sysToken) || config.auth.accessToken;
+
+  return `${tokenType} ${accessToken}`;
+};
+request.base.interceptError = data => {
+  if (data.code === 401) {
+    if (!firstEnter) {
+      notification.warning({ message: '登录已过期，请重新登录' });
+    }
+    firstEnter = false;
+    storage.remove([
+      storageKeys.sysToken,
+      storageKeys.sysTokenExpire,
+      storageKeys.sysTokenType,
+      storageKeys.userInfo,
+    ]);
+    history.replace('/login');
+    return false;
+  }
+  return true;
+};
+
+export default request;
